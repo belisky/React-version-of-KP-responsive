e1f00a35e2f5e2540422b3e2e4e2ce562530808a
bel <denoblesnobility2@gmail.com>
2021-10-29 19:57:59 +0100

    basic setup and styling begins

--- kp-responsive/src/App.js
+++ kp-responsive/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+ 
+import './Styles.css';
+import done from "./done.jpg";
+import brain from "./brain.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <header className="bg-light text-center">
+         <p>Lorem, ipsum dolor.</p>
+         <h1>Responsive layouts don't have to be a struggle</h1>
+       </header>
+       <section className="bg-white">
+         <h2 className="text-center">Quality Designs</h2>
+         <div className="split">
+           <div>
+             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint veniam similique corporis dolorem eaque odit eos, ipsam voluptates reiciendis rerum inventore est. Est, cupiditate reprehenderit.</p>
+           </div>
+           <div>
+             <p>Dolor rerum excepturi odio quis? Cumque repellendus ratione nihil repellat suscipit corporis at veniam, soluta labore, adipisci expedita excepturi. Aspernatur quia magni hic est excepturi.</p>
+           </div>
+         </div>
+        </section>
+        <section className="bg-dark">
+          <div className="split">
+            <div>
+              <h2>Made custom for you</h2>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero in fugit enim eius aliquam minima.</p>
+            </div>
+            <div>
+              <img src={brain} alt="some picture keke" />
+            </div>
+          </div>
+        </section>
+        <section className="bg-accent">
+          <div className="split">
+            <div>
+              <img src={done} alt="some another picture bi keke" />
+            </div>
+            <div>
+              <h2>Something sweet</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo facilis in non culpa. Minima porro esse illo ducimus tempora totam.</p>
+            </div>
+          </div>
+        </section>
     </div>
   );
 }
