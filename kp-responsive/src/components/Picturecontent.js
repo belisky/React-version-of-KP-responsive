e9aa6d4e09038e878c1770c07921c5926fdd5952
bel <denoblesnobility2@gmail.com>
2021-11-02 19:34:09 +0000

    some final touches, got to learn that divs make styling easier

--- kp-responsive/src/components/Picturecontent.js
+++ kp-responsive/src/components/Picturecontent.js
@@ -8,12 +8,12 @@ const Picturecontent = () => {
             <section className="bg-dark">
               <div className="container">
                 <div className="split">
-                  <div>
+                  <div className="split_content">
                     <h2>Made custom for you</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero in fugit enim eius aliquam minima.</p>
-                  </div>
-                  <div>
-                    <img src={kanban} alt="some picture keke" />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro alias quibusdam repudiandae officiis beatae. Itaque?</p>
+                   </div>
+                  <div className="split_img">
+                    <img src={kanban} alt="ook you people,some picture keke" />
                   </div>
                 </div>
               </div>
@@ -21,13 +21,13 @@ const Picturecontent = () => {
             <section className="bg-primary">
               <div className="container">
                 <div className="split">
-                  <div>
+                  <div className="split_img">
                     <img src={brain} alt="some another picture bi keke" />
                   </div>
-                  <div>
+                  <div className="split_content">
                     <h2>Something sweet</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo facilis in non culpa. Minima porro esse illo ducimus tempora totam.</p>
-                  </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora odio sit similique inventore commodi eligendi?</p>
+                    </div>
                 </div>
               </div>
             </section>
